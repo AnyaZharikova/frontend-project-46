e00af899cf46ce194b8d9ecfe749e89a0563a7a5
Anya Zharikova <kannie327@gmail.com>
2025-01-08 17:20:33 +0200

    fix gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,11 +4,11 @@ import { program } from 'commander';
 
 program
   .name('gendiff')
+  .version('1.0.0')  
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .version('1.0.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .argument('<filepath1>', 'path to the first file')
+  .argument('<filepath2>', 'path to the second file')
   .option('-f, --format [type]', 'output format');
 
 program.parse();
