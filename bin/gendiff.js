d4dd841fccb9a1ebd971ee6d26fe207a3217cdcc
Anya Zharikova <kannie327@gmail.com>
2025-01-15 15:59:11 +0200

    add comparison of two files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import { makeObject, genDiff } from '../src/index.js';
 
 program
   .name('gendiff')
-  .version('1.0.0')  
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .argument('<filepath1>', 'path to the first file')
   .argument('<filepath2>', 'path to the second file')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(makeObject(filepath1), makeObject(filepath2)));
+  });
 
 program.parse();
