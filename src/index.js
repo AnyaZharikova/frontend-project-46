2ae3d59e2b3c50bb6ac0d01e744e742e460bfc10
Anya Zharikova <kannie327@gmail.com>
2025-01-17 21:46:15 +0200

    Fix functions and move parser

--- src/index.js
+++ src/index.js
@@ -1,51 +1,49 @@
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
-import yaml from 'js-yaml';
+import parse from './parsers.js';
 
-const getAbsolutePath = (file) => path.resolve(process.cwd(file), file);
+const getAbsolutePath = (file) => path.resolve(process.cwd(), file);
 
 const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
 
-const getExtension = (file) => path.extname(getAbsolutePath(file));
-
-const makeObject = (file) => {
-  const ext = getExtension(file);
-
-  switch (ext) {
-    case '.json':
-      return JSON.parse(readFile(file));
-    case '.yml':
-    case '.yaml':
-      return yaml.load(readFile(file));
-    default:
-      throw new Error(`${ext} is not suppurted extension!`);
-  }
-};
+const getExtension = (file) => path.extname(file);
 
 const genDiff = (file1, file2) => {
-  const keys = _.sortBy(Object.keys({ ...file1, ...file2 }));
+  const dataOfFile1 = readFile(file1);
+  const dataOfFile2 = readFile(file2);
+
+  const extOfFile1 = getExtension(file1);
+  const extOfFile2 = getExtension(file2);
+
+  const parsedFile1 = parse(dataOfFile1, extOfFile1);
+  const parsedFile2 = parse(dataOfFile2, extOfFile2);
+
+  const keys = _.sortBy(Object.keys({ ...parsedFile1, ...parsedFile2 }));
   const indent = '  ';
   const matched = ' ';
   const added = '+';
   const changed = '-';
 
   const getDiff = (key) => {
+    const value1 = parsedFile1[key];
+    const value2 = parsedFile2[key];
+
     switch (true) {
-      case !Object.hasOwn(file2, key):
-        return `${indent}${changed} ${key}: ${file1[key]}`;
-      case !Object.hasOwn(file1, key):
-        return `${indent}${added} ${key}: ${file2[key]}`;
-      case file1[key] !== file2[key]:
-        return `${indent}${changed} ${key}: ${file1[key]}\n${indent}${added} ${key}: ${file2[key]}`;
+      case !Object.hasOwn(parsedFile2, key):
+        return `${indent}${changed} ${key}: ${value1}`;
+      case !Object.hasOwn(parsedFile1, key):
+        return `${indent}${added} ${key}: ${value2}`;
+      case parsedFile1[key] !== parsedFile2[key]:
+        return `${indent}${changed} ${key}: ${value1}\n${indent}${added} ${key}: ${value2}`;
       default:
-        return `${indent}${matched} ${key}: ${file1[key]}`;
+        return `${indent}${matched} ${key}: ${value1}`;
     }
   };
 
-  const result = keys.map(getDiff);
+  const result = keys.map(getDiff).join('\n');
 
-  return `{\n${result.join('\n')}\n}`;
+  return `{\n${result}\n}`;
 };
 
-export { makeObject, genDiff };
+export default genDiff;
