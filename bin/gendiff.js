31f2bd18ad908a00643d259b3a3af6429c566382
Anya Zharikova <kannie327@gmail.com>
2025-01-17 21:48:19 +0200

    Fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import { makeObject, genDiff } from '../src/index.js';
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
@@ -12,7 +12,7 @@ program
   .argument('<filepath2>', 'path to the second file')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(makeObject(filepath1), makeObject(filepath2)));
+    console.log(genDiff(filepath1, filepath2));
   });
 
 program.parse();
