e80ab7be2a5f3ffb93dd85bb7dbb64ff86de738a
Anya Zharikova <kannie327@gmail.com>
2025-01-17 21:39:45 +0200

    add yaml-files testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,30 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { genDiff, makeObject } from '../src/index.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const fixturesPath = path.join(__dirname, '..', '__fixtures__');
 
-test('return difference', () => {
-  const filepath1 = path.join(fixturesPath, 'file1.json');
-  const filepath2 = path.join(fixturesPath, 'file2.json');
-  const expectedResultPath = path.join(fixturesPath, 'expected_result.txt');
-  const expectedResult = fs.readFileSync(expectedResultPath, 'utf-8').trim();
-  const actualResult = genDiff(makeObject(filepath1), makeObject(filepath2));
+describe('genDiff', () => {
+  test('compare two JSON-files', () => {
+    const filepath1 = path.join(fixturesPath, 'file1.json');
+    const filepath2 = path.join(fixturesPath, 'file2.json');
+    const expectedResultPath = path.join(fixturesPath, 'expected_result.txt');
+    const expectedResult = fs.readFileSync(expectedResultPath, 'utf-8').trim();
+    const actualResult = genDiff(filepath1, filepath2);
 
-  expect(actualResult).toEqual(expectedResult);
-});
+    expect(actualResult).toEqual(expectedResult);
+  });
+
+  test('compare two YAML-files', () => {
+    const filepath1 = path.join(fixturesPath, 'file1.yaml');
+    const filepath2 = path.join(fixturesPath, 'file2.yaml');
+    const expectedResultPath = path.join(fixturesPath, 'expected_result.txt');
+    const expectedResult = fs.readFileSync(expectedResultPath, 'utf-8').trim();
+    const actualResult = genDiff(filepath1, filepath2);
+
+    expect(actualResult).toEqual(expectedResult);
+  });
+})
