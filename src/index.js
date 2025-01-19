03139eeeb23dccb394ca48f40f886c18037c0661
Anya Zharikova <kannie327@gmail.com>
2025-01-19 22:23:55 +0200

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
+import makeComparisonTree from './compareData.js';
+import applyFormat from './formatters/index.js';
 
 const getAbsolutePath = (file) => path.resolve(process.cwd(), file);
 
@@ -9,7 +10,7 @@ const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8
 
 const getExtension = (file) => path.extname(file);
 
-const genDiff = (file1, file2) => {
+const genDiff = (file1, file2, format = 'stylish') => {
   const dataOfFile1 = readFile(file1);
   const dataOfFile2 = readFile(file2);
 
@@ -18,32 +19,10 @@ const genDiff = (file1, file2) => {
 
   const parsedFile1 = parse(dataOfFile1, extOfFile1);
   const parsedFile2 = parse(dataOfFile2, extOfFile2);
-
-  const keys = _.sortBy(Object.keys({ ...parsedFile1, ...parsedFile2 }));
-  const indent = '  ';
-  const matched = ' ';
-  const added = '+';
-  const changed = '-';
-
-  const getDiff = (key) => {
-    const value1 = parsedFile1[key];
-    const value2 = parsedFile2[key];
-
-    switch (true) {
-      case !Object.hasOwn(parsedFile2, key):
-        return `${indent}${changed} ${key}: ${value1}`;
-      case !Object.hasOwn(parsedFile1, key):
-        return `${indent}${added} ${key}: ${value2}`;
-      case parsedFile1[key] !== parsedFile2[key]:
-        return `${indent}${changed} ${key}: ${value1}\n${indent}${added} ${key}: ${value2}`;
-      default:
-        return `${indent}${matched} ${key}: ${value1}`;
-    }
-  };
-
-  const result = keys.map(getDiff).join('\n');
-
-  return `{\n${result}\n}`;
+  //console.log(parsedFile1, parsedFile2)
+  const diffTree = makeComparisonTree(parsedFile1, parsedFile2);
+  //console.log(diffTree);
+  return applyFormat(diffTree, format);
 };
 
 export default genDiff;
