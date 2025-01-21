f6cffc78fe3960e6a8c1eee5b12fd1f502cc54cd
Anya Zharikova <kannie327@gmail.com>
2025-01-21 19:02:13 +0200

    Fix index.js for new format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
 import makeStylish from './stylish.js';
+import makePlain from './plain.js';
 
 const applyFormat = (diffTree, format) => {
   switch (format) {
     case 'stylish':
       return makeStylish(diffTree);
+    case 'plain':
+      return makePlain(diffTree);
     default:
       throw new Error(`Unexpected format: ${format}!`);
   }
