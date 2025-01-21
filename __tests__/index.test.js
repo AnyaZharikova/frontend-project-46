bf387d59d1bca3bf163bcba17a6b8de775f1477e
Anya Zharikova <kannie327@gmail.com>
2025-01-21 18:58:21 +0200

    Fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,23 +7,45 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const fixturesPath = path.join(__dirname, '..', '__fixtures__');
 
-describe('genDiff', () => {
-  test('compare two JSON-files', () => {
-    const filepath1 = path.join(fixturesPath, 'file1.json');
-    const filepath2 = path.join(fixturesPath, 'file2.json');
-    const expectedResultPath = path.join(fixturesPath, 'expected_result.txt');
+const formats = ['stylish', 'plain'];
+
+describe.each(formats)('genDiff JSON in format: %s', (format) => {
+  let filepath1;
+  let filepath2;
+
+  beforeEach(() => {
+    filepath1 = path.join(fixturesPath, 'file1.json');
+    filepath2 = path.join(fixturesPath, 'file2.json');
+  });
+
+  test(`output in ${format}`, () => {
+    const expectedResultPath = path.join(
+      fixturesPath,
+      format === 'stylish' ? 'expected_result.txt' : 'exp_result_plain.txt',
+    );
     const expectedResult = fs.readFileSync(expectedResultPath, 'utf-8').trim();
-    const actualResult = genDiff(filepath1, filepath2);
+    const actualResult = genDiff(filepath1, filepath2, format);
 
     expect(actualResult).toEqual(expectedResult);
   });
+});
 
-  test('compare two YAML-files', () => {
-    const filepath1 = path.join(fixturesPath, 'file1.yaml');
-    const filepath2 = path.join(fixturesPath, 'file2.yaml');
-    const expectedResultPath = path.join(fixturesPath, 'expected_result.txt');
+describe.each(formats)('genDiff YAML in format: %s', (format) => {
+  let filepath1;
+  let filepath2;
+
+  beforeEach(() => {
+    filepath1 = path.join(fixturesPath, 'file1.yaml');
+    filepath2 = path.join(fixturesPath, 'file2.yaml');
+  });
+
+  test(`output in ${format}`, () => {
+    const expectedResultPath = path.join(
+      fixturesPath,
+      format === 'stylish' ? 'expected_result.txt' : 'exp_result_plain.txt',
+    );
     const expectedResult = fs.readFileSync(expectedResultPath, 'utf-8').trim();
-    const actualResult = genDiff(filepath1, filepath2);
+    const actualResult = genDiff(filepath1, filepath2, format);
 
     expect(actualResult).toEqual(expectedResult);
   });
