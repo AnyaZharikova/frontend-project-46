1deee691f3948e61d272f7dde3aebfe105a2d3ba
Anya Zharikova <kannie327@gmail.com>
2025-01-23 14:39:34 +0200

    Rewrite tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,45 +7,39 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const fixturesPath = path.join(__dirname, '..', '__fixtures__');
 
-const formats = ['stylish', 'plain'];
+const extensions = ['.json', '.yaml'];
 
-describe.each(formats)('genDiff JSON in format: %s', (format) => {
-  let filepath1;
-  let filepath2;
-
-  beforeEach(() => {
-    filepath1 = path.join(fixturesPath, 'file1.json');
-    filepath2 = path.join(fixturesPath, 'file2.json');
-  });
-
-  test(`output in ${format}`, () => {
-    const expectedResultPath = path.join(
-      fixturesPath,
-      format === 'stylish' ? 'expected_result.txt' : 'exp_result_plain.txt',
-    );
+describe.each(extensions)('genDiff of extensions: %s', (extension) => {
+  test('output in stylish', () => {
+    const filepath1 = path.join(fixturesPath, `file1${extension}`);
+    const filepath2 = path.join(fixturesPath, `file2${extension}`);
+    const expectedResultPath = path.join(fixturesPath, 'result_stylish.txt');
     const expectedResult = fs.readFileSync(expectedResultPath, 'utf-8').trim();
-    const actualResult = genDiff(filepath1, filepath2, format);
+    const actualResult = genDiff(filepath1, filepath2, 'stylish');
 
     expect(actualResult).toEqual(expectedResult);
   });
 });
 
-describe.each(formats)('genDiff YAML in format: %s', (format) => {
-  let filepath1;
-  let filepath2;
-
-  beforeEach(() => {
-    filepath1 = path.join(fixturesPath, 'file1.yaml');
-    filepath2 = path.join(fixturesPath, 'file2.yaml');
-  });
-
-  test(`output in ${format}`, () => {
-    const expectedResultPath = path.join(
-      fixturesPath,
-      format === 'stylish' ? 'expected_result.txt' : 'exp_result_plain.txt',
-    );
+describe.each(extensions)('genDiff of extensions: %s', (extension) => {
+  test('output in plain', () => {
+    const filepath1 = path.join(fixturesPath, `file1${extension}`);
+    const filepath2 = path.join(fixturesPath, `file2${extension}`);
+    const expectedResultPath = path.join(fixturesPath, 'result_plain.txt');
     const expectedResult = fs.readFileSync(expectedResultPath, 'utf-8').trim();
-    const actualResult = genDiff(filepath1, filepath2, format);
+    const actualResult = genDiff(filepath1, filepath2, 'plain');
+
+    expect(actualResult).toEqual(expectedResult);
+  });
+});
+
+describe.each(extensions)('genDiff of extensions: %s', (extension) => {
+  test('output in json', () => {
+    const filepath1 = path.join(fixturesPath, `file1${extension}`);
+    const filepath2 = path.join(fixturesPath, `file2${extension}`);
+    const expectedResultPath = path.join(fixturesPath, 'result_json.txt');
+    const expectedResult = fs.readFileSync(expectedResultPath, 'utf-8').trim();
+    const actualResult = genDiff(filepath1, filepath2, 'json');
 
     expect(actualResult).toEqual(expectedResult);
   });
